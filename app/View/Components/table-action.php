9f01dec99c1003e726eabc58b7a7ea914a0fdc78
wahdam <wahdam.amarulloh@gmail.com>
2021-09-22 20:16:09 +0700

    create config file

--- app/View/Components/table-action.php
+++ app/View/Components/table-action.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class table-action extends Component
+class tableaction extends Component
 {
     /**
      * Create a new component instance.
