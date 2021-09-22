6cf031fd5cd2074698b1057ea0c6bb275fcbf2d4
wahdam <wahdam.amarulloh@gmail.com>
2021-09-22 21:33:20 +0700

    remove test in controller

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -12,21 +12,6 @@ class Dashboard extends Controller
     public function main()
     {
 
-        $sid = env('TWELIO_SID');
-        $token = env('TWELIO_TOKEN');
-        $client = new Client($sid, $token);
-
-        $client->messages->create(
-            // the number you'd like to send the message to
-            'whatsapp:+6289637058723',
-            [
-                // A Twilio phone number you purchased at twilio.com/console
-                'from' => 'whatsapp:+14155238886',
-                // the body of the text message you'd like to send
-                'body' => 'Test Custom'
-            ]
-        );
-
         $personCount = DB::table('person')->count();
         $vehicleCount = DB::table('vehicle')->count();
         $todayVehicle = DB::table('vehicle')->whereDate('effective_date', today())->count();
