72b75559ccd3865789a636026de3398f0ba419f4
wahdam <wahdam.amarulloh@gmail.com>
2021-09-28 22:39:13 +0700

    add ability to insert text on click hint

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -10,7 +10,7 @@
         <div class="w-2/3 flex flex-col mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
             <div class="bg-gray-200 text-gray-700 text-lg px-6 py-4">
                 <div class="flex justify-between items-center">Content
-                    <div class="inline text-sm right">{{ $content->created_at->toDateString() }}</div>
+                    <div class="inline text-sm right">{{ $content->created_at ?? now()->toDateString() }}</div>
                 </div>
             </div>
 
@@ -18,8 +18,8 @@
                 <form action="{{ route('template.store') }}" method="post">
                     @csrf
                     <div class="border rounded-lg p-4 bg-gray-200">
-                        <textarea name="content" value="{{ $content->content }}"
-                            class="textarea h-24 textarea-bordered w-full bg-transparent border-0 border-opacity-0 shadow-none focus:outline-none focus:ring-offset-0 focus:border-0">{{ $content->content }}</textarea>
+                        <textarea name="content" value="{{ $content->content ?? '' }}"
+                            class="textarea h-24 textarea-bordered w-full bg-transparent border-0 border-opacity-0 shadow-none focus:outline-none focus:ring-offset-0 focus:border-0">{{ $content->content ?? '' }}</textarea>
                     </div>
                     <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-1"
                         type="submit">
@@ -31,11 +31,9 @@
             <div class="bg-gray-200 px-6 py-4">
                 <div class="uppercase text-xs text-gray-600 font-bold">Hint</div>
                 <div class="flex items-center pt-3">
-                    <div class="ml-4">
-                        <span
-                            class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-indigo-100 bg-indigo-700 rounded">Name</span>
-                        <span
-                            class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-indigo-100 bg-indigo-700 rounded">Duedate</span>
+                    <div class="ml-4 hint-container">
+                        <x-hint-button data-value="$name">Name</x-hint-button>
+                        <x-hint-button data-value="$duedate">Duedate</x-hint-button>
                     </div>
                 </div>
             </div>
@@ -43,4 +41,49 @@
         {{-- End::Content --}}
     </div>
     </div>
+
+    @push('scripts')
+    <script>
+        function insertAtCursor(myField, myValue) {
+        //IE support
+        if (document.selection) {
+            myField.focus();
+            sel = document.selection.createRange();
+            sel.text = myValue;
+        }
+        // Microsoft Edge
+        else if(window.navigator.userAgent.indexOf("Edge") > -1) {
+          var startPos = myField.selectionStart; 
+          var endPos = myField.selectionEnd; 
+              
+          myField.value = myField.value.substring(0, startPos)+ myValue 
+                 + myField.value.substring(endPos, myField.value.length); 
+          
+          var pos = startPos + myValue.length;
+          myField.focus();
+          myField.setSelectionRange(pos, pos);
+        }
+        //MOZILLA and others
+        else if (myField.selectionStart || myField.selectionStart == '0') {
+            var startPos = myField.selectionStart;
+            var endPos = myField.selectionEnd;
+            myField.value = myField.value.substring(0, startPos)
+                + myValue
+                + myField.value.substring(endPos, myField.value.length);
+        } else {
+            myField.value += myValue;
+        }
+    }
+
+    let container = document.getElementsByClassName('hint-container')[0];
+    let hint = Array.from(container.children);
+
+    hint.forEach(element => {
+        element.onclick = function() {
+            insertAtCursor(document.getElementsByTagName('textarea')[0], this.getAttribute('data-value'));
+        }
+    });
+
+    </script>
+    @endpush
 </x-app-layout>
