33e3205a8c93711f7e0e79e5be60077aff98ed91
wahdam <wahdam.amarulloh@gmail.com>
2022-02-10 09:51:02 +0700

    remove button

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -7,14 +7,6 @@
 
     <div class="py-12 ">
         <div class="w-full mx-auto sm:px-6 lg:px-8">
-
-            <div class="bg-white border-l-2 border-slate-500 overflow-hidden shadow-sm sm:rounded-lg mb-3 p-6">
-                <span>
-                    <x-button onclick="livewire.emit('toggleModal',{'name':'CreatePerson'})">New
-                    </x-button>
-                </span>
-            </div>
-
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-2 border-slate-500">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <livewire:feedback-table />
